a683a9dd70c8aa0907b91b126ffd0903b7fa51d3
Ivan Volodartsev <untrupus@gmail.com>
2020-09-17 14:03:44 +0600

    add show image mount

--- src/containers/Show/Show.js
+++ src/containers/Show/Show.js
@@ -6,7 +6,12 @@ import './Show.css';
 const FETCH_SHOW = "FETCH_SHOW";
 
 const initialState = {
-    show: {}
+    show: {
+        network: {name: '', country: {name: ''}},
+        genres: [],
+        runtime: '',
+        image: {original: ''}
+    }
 };
 
 const reducer = (state, action) => {
@@ -35,10 +40,35 @@ const Show = props => {
     }, [props.match.params.id]);
 
     const summary = parse("" + state.show.summary);
+
+    let image;
+    if (state.show.image === null) {
+        image = require('../../no-img.png');
+    } else {
+        image = state.show.image.original;
+    }
+
     return (
-        <div className="container">
+        <div className="container show">
             <h3>{state.show.name}</h3>
-            {summary}
+            <div className="showInfo">
+                <div className="showDescription inner">
+                    <img src={image}
+                         alt="showImage"
+                         className="image"
+                    />
+                </div>
+                <div className="showAbout inner">
+                    <p><span>Type:</span> {state.show.type}</p>
+                    <p><span>Network:</span> {state.show.network.name}</p>
+                    <p><span>Country:</span> {state.show.network.country.name}</p>
+                    <p><span>Language:</span> {state.show.language}</p>
+                    <p><span>Status:</span> {state.show.status}</p>
+                    <p><span>Runtime:</span> {state.show.runtime}</p>
+                    <p><span>Genres:</span> {state.show.genres.join(' ')}</p>
+                    {summary}
+                </div>
+            </div>
         </div>
     );
 };
